5209d7dc45c9199ff48f1df49c979d6dca97f36b
david <davbros88@hotmail.com>
2016-03-14 13:32:21 -0500

    push

--- Tutorial/program.js
+++ Tutorial/program.js
@@ -1,3 +1,13 @@
-setTimeout(300, ms, {
-    success: print('TIMED OUT!')
-});
+var promise= new Promise(function(resolve,reject) {
+   
+}).then(function(success) {
+    console.log('I FIRED');
+    console.log(success)
+}) .then(function onRejected(error){
+    console.log('I DID NOT FIRE');
+    console.log(error);
+}); promise().then((console.log),onRejected);
+    
+    
+    
+    
